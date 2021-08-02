dae74c1eef1c781cf96ccc0c27b5f6dbd4645e54
Hari Limaye <hari.limaye@arm.com>
2021-08-02 23:22:51 +0100

    Changes for presentation

--- my_startup/startup_K66F.c
+++ my_startup/startup_K66F.c
@@ -1,19 +1,20 @@
 #include "system_MK66F18.h"
+#include <stdint.h>
 
-extern unsigned int _etext;
-extern unsigned int _sdata;
-extern unsigned int _edata;
-extern unsigned int _sbss;
-extern unsigned int _ebss;
-
-#define SRAM_START   0x1FFF0000
-#define SRAM_SIZE    (256 * 1024U) // 256KB
-#define SRAM_END     ((SRAM_START) + (SRAM_SIZE))
-
-#define STACK_TOP SRAM_END
+extern uint32_t _stack_top;
+extern uint32_t _etext;
+extern uint32_t _sdata;
+extern uint32_t _edata;
+extern uint32_t _sbss;
+extern uint32_t _ebss;
 
 // Prototype of main function
 void main(void);
+
+void Default_Handler(void)
+{
+    while(1);
+}
 
 void Reset_Handler(void);
 void NMI_Handler(void) __attribute__ ((weak, alias("Default_Handler")));
@@ -26,67 +27,53 @@ void DebugMonitor_Handler(void) __attribute__ ((weak, alias("Default_Handler")))
 void PendableSrvReq_Handler(void) __attribute__ ((weak, alias("Default_Handler")));
 void SysTick_Handler(void) __attribute__ ((weak, alias("Default_Handler")));
 
-unsigned int * vector_table[256] __attribute__((section(".isr_vector"))) = {
-    (unsigned int *)    STACK_TOP,      // stack pointer
-    (unsigned int *)    &Reset_Handler,
-    (unsigned int *)    &NMI_Handler,
-    (unsigned int *)    &HardFault_Handler,
-    (unsigned int *)    &MemManage_Handler,
-    (unsigned int *)    &BusFault_Handler,
-    (unsigned int *)    &UsageFault_Handler,
-    (unsigned int *)    0,
-    (unsigned int *)    0,
-    (unsigned int *)    0,
-    (unsigned int *)    0,
-    (unsigned int *)    &SVCall_Handler,
-    (unsigned int *)    &DebugMonitor_Handler,
-    (unsigned int *)    0,
-    (unsigned int *)    &PendableSrvReq_Handler,
-    (unsigned int *)    &SysTick_Handler
+uint32_t * vector_table[256] __attribute__((section(".isr_vector"))) = {
+    (uint32_t *)    &_stack_top,      // stack pointer
+    (uint32_t *)    &Reset_Handler,
+    (uint32_t *)    &HardFault_Handler,
+    (uint32_t *)    &MemManage_Handler,
+    (uint32_t *)    &BusFault_Handler,
+    (uint32_t *)    &UsageFault_Handler,
+    (uint32_t *)    0,
+    (uint32_t *)    0,
+    (uint32_t *)    0,
+    (uint32_t *)    0,
+    (uint32_t *)    &SVCall_Handler,
+    (uint32_t *)    &DebugMonitor_Handler,
+    (uint32_t *)    0,
+    (uint32_t *)    &PendableSrvReq_Handler,
+    (uint32_t *)    &SysTick_Handler
 };
 
-unsigned int flash_config[4] __attribute__((section(".flash_config"))) = {
+uint32_t flash_config[4] __attribute__((section(".flash_config"))) = {
     0xFFFFFFFF,
     0xFFFFFFFF,
     0xFFFFFFFF,
     0xFFFFFFFE
 };
 
-void Default_Handler(void)
-{
-    while(1);
-}
-
 void Reset_Handler(void)
 {
     /* Disable Watchdog */
     SystemInit();
 
-    /* Copy data belonging to the `.data` section from its
-    * load time position on flash (ROM) to its run time position
-    * in SRAM.
-    */
-    unsigned int *data_flash_start = &_etext;
-    unsigned int *data_ram_start_p = &_sdata;
-    unsigned int *data_ram_end_p = &_edata;
+    /* Copy data from FLASH to SRAM */
+    uint32_t *data_flash_start = &_etext;
+    uint32_t *data_ram_start_p = &_sdata;
+    uint32_t *data_ram_end_p = &_edata;
 
     while (data_ram_start_p < data_ram_end_p) {
         *data_ram_start_p++ = *data_flash_start++;
     }
 
-    /* Initialize data in the `.bss` section to zeros.
-     */
-    unsigned int *bss_start_p = &_sbss;
-    unsigned int *bss_end_p = &_ebss;
+    /* Initialize data in the `.bss` section to zeros. */
+    uint32_t *bss_start_p = &_sbss;
+    uint32_t *bss_end_p = &_ebss;
 
     while (bss_start_p < bss_end_p) {
         *bss_start_p++ = 0;
     }
 
-    /* Call the main function. */
     main();
-
-    /* Loop */
-    while(1);
 }
 
